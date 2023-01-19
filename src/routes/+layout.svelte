5f368dce6c5377a9b7335b7b1abea3e67b9a1e13
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2023-01-19 10:59:48 +0800

    Add basic 3d in hero

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import Navbar from "./Navbar.svelte";
 </script>
 
-<div class="font-comic">
+<div class="font-comic bg-gradient-to-r from-yellow-200 to-orange-400 min-h-screen">
     <Navbar />
     <slot/>
 </div>
--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,3 +1,18 @@
-<div class="bg-amber-600 h-full w-full">
-    This is the new hero section
+<script>
+    import * as THREE from "three";
+    import {Canvas, Mesh, PerspectiveCamera, onFrame} from 'svelte-cubed';
+
+    let spin = 0;
+
+    onFrame(() => {
+        spin = spin + 0.01;
+        console.log(spin);
+    })
+</script>
+
+<div class="h-full w-full">
+    <Canvas alpha={true}>
+        <Mesh geometry={new THREE.BoxGeometry()} rotation={[0, spin, 0]}/>
+        <PerspectiveCamera position={[1, 10, 10]}/>
+    </Canvas>
 </div>
