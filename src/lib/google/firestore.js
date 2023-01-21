5139e78efb27fc7d2aa71beb034fc77a57fb6f62
Felix Wong <felixwongyuenlam@gmail.com>
2023-01-21 23:20:44 +0800

    add dotenv back

--- src/lib/google/firestore.js
+++ src/lib/google/firestore.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc, setDoc, doc } from "firebase/firestore";
-// import * as dotenv from "dotenv";
-// dotenv.config();
+import * as dotenv from "dotenv";
+dotenv.config();
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
