889357a1ad4fbebdc4d092b3e85e15cb43a155e5
Felix Wong <felixwongyuenlam@gmail.com>
2023-01-21 16:55:47 +0800

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
 
 	kit: {
 		// default options are shown
-		adapter: adapter({
+		adapter: vercel({
 			// if true, will deploy the app using edge functions
 			// (https://vercel.com/docs/concepts/functions/edge-functions)
 			// rather than serverless functions
