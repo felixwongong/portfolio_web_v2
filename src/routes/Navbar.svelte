cfb59d6f58af2fc41ca44c84d4133fe758726af8
Felix Wong <felixwongyuenlam@gmail.com>
2023-01-26 12:01:56 +0800

    commit

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -25,7 +25,7 @@
     });
 </script>
 
-<div class="navbar bg-base-100">
+<div class="absolute navbar bg-base-100">
     <div class="navbar-start">
         <div class="dropdown">
             <label tabindex="0" class="btn btn-ghost lg:hidden">
