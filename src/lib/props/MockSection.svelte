f6de1eb1260ccf007c83812dc7527e893da658fb
Felix Wong <felixwongyuenlam@gmail.com>
2023-01-27 23:24:52 +0800

    fix ugly bg

--- src/lib/props/MockSection.svelte
+++ src/lib/props/MockSection.svelte
@@ -4,7 +4,7 @@
 
 <Section class="md:aspect-video md:py-0 md:my-10">
     <div class="mockup-window border bg-base-200 h-full">
-        <div class="bg-primary h-full  {$$props.class}">
+        <div class="bg-base-100 dark:bg-primary h-full  {$$props.class}">
             <slot/>
         </div>
     </div>
